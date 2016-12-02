a812a8420fbfcc60c5d73ab94c2ceb6f374d2966
aditi-garg1 <aditigarg.garg2@gmail.com>
2016-12-02 16:17:18 +0530

    Update graph.py

--- graph.py
+++ graph.py
@@ -2,7 +2,7 @@ from facepy import GraphAPI
 import json
 from json2html import *
 import webbrowser
-graph= GraphAPI('185059841956329|JGhapIO9wKIffMzEznCgy--K0us')
+graph= GraphAPI('EAAL76SmH3vMBAIUClQGUlfBml6qbGRYEZBOIA72YCrKMin8iCjmxEJvvZBn3M5ccxLiI6mstuferOIXHiHj4kRvuXaawTHr3XydoFvZAJAVZBEIcbdt4uK2dhgLZAuV3UBU7VGcypB4poOfKROJpuybEZC84Si1qkbGeKIIMgKqgZDZD')
 
 print("Please enter the page-id  " )
 PageId=raw_input()
